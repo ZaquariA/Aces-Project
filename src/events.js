d88ba9edc8b538e7a8b591266b51aaffe46af7c3
zaquari_andl <zaquariandl043@gmail.com>
2023-12-05 07:42:43 -0600

    worked on crud for section 1

---
+++ src/events.js
@@ -0,0 +1,55 @@
+// EVENTLISTENERS
+
+// NEW GAME BUTTON
+function startGame() {
+    getEl('#btn_new_game').addEventListener('click', () => {
+      console.log('start')
+      shuffleCards()
+        .then(data => {
+          deckId = data.deck_id
+  
+          cleanDataForNewGame()
+          // showBackOfCards()
+    
+          getCards(deckId, 2)
+            .then(cards => showCardOnTable(cards, "#cards_player"))
+        })
+    })
+  }
+  
+  function cleanDataForNewGame() {
+    cleanTable()
+  
+    userDone = false
+    getEl('#game_status').textContent = "In process..."
+  }
+  
+  // HIT BUTTON
+  function callHit() {
+    getEl('#btn_hit').addEventListener('click', () => {
+  
+      getCards(deckId, 1)
+        .then(cards => showCardOnTable(cards, "#cards_player"))
+    })
+  }
+  
+  // STAND BUTTON
+  function callStand() {
+    getEl('#btn_stand').addEventListener('click', () => {
+      console.log('STAND')
+      userDone = true
+      getCards(deckId, 1)
+        .then(cards => showCardOnTable(cards, "#cards_casino"))
+    })
+  }
+  
+  // HIT BUTTON
+  function callDealerHit() {
+    console.log('callDealerHit')
+      getCards(deckId, 1)
+        .then(cards => showCardOnTable(cards, "#cards_casino"))
+  }
+  
+  startGame()   
+  callHit()
+  callStand()
